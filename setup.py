ee3fb349ed563433b128ae92aac347ed2ccbd4b1
vishnushankar1 <vishnushankar123123@gmail.com>
2025-06-13 10:34:05 +0530

    rquiremnest file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vishnu Shankar",
+    author_email="vishnushankar123123@gmail.com",
+    packages=find_packages()
+)
